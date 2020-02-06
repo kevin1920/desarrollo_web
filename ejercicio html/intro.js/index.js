3b5087cb2e23f1764a855970764e285dcd31f1bc
pineappledavs <kevinloco1920@gmail.com>
2020-02-06 07:43:31 -0500

    Clase de js

---
+++ ejercicio html/intro.js/index.js
@@ -0,0 +1,46 @@
+console.log("Hola Mundo")
+console.info("Info")
+console.error("Error")
+console.warn("Alerta")
+
+let nombre = "Kevin"
+let Apellido = "Gonzalez"
+const edad = 50
+var color = "blanco"
+
+let altura
+
+let esta_vivo = true
+
+let nombreCompleto1 = nombre+' '+Apellido
+let nombreCompleto2 = `${nombre} ${Apellido}`
+console.log('Nombre completo',nombreCompleto1)
+console.log('Nombre completo',nombreCompleto2)
+
+console.log('Altura', altura)
+
+if(edad>49){
+    let nombre = "Otro nombre"
+    var color = "otro color"
+    console.log("Dentro de if", nombre, color)
+}
+
+if(Alura == undefined){
+    console.log("Primera Validacion")
+
+}
+
+if(altura){
+    console.log("Segunda validacion")
+
+}else{
+    console.log("Segunda validaqcion")
+}
+
+
+let num1 = parseInt("1")
+let num2 = parseFloat(true)
+console.log(num1+num2)
+/*
+console.log(nombre)
+console.log('Altura', altura)*/
